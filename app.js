59872edc6bdc5f0468da3aeb53fae8bca1552566
keitheck <eckertkeith@gmail.com>
2017-12-08 14:42:53 -0800

    problem 2 working

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ function sum(a,b){
   var message = 'The sum of ' + a + ' and ' + b + ' is ' + theSum + '.';
   return [theSum, message];
 }
-sum(4,7);
+
 // TODO: Here is the test for sum(); uncomment it to run it
 testSum(4, 7);
 
@@ -32,12 +32,14 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // TODO: Write your code here
-function multiply(a,b){ //eslint-disable-line
-
+function multiply(a,b){
+  var multNum = a * b;
+  var multMessage = 'The product of ' + a + ' and ' + b + ' is ' + multNum + '.';
+  return [multNum, multMessage];
 }
-
+multiply(5,9);
 // TODO: Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
