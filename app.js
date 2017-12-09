a4c205976786f8f55f205a0c642d3eb5f5543d2a
keitheck <eckertkeith@gmail.com>
2017-12-08 17:26:16 -0800

    problem 4 working

--- app.js
+++ app.js
@@ -75,7 +75,8 @@ testSumAndMultiply(4,7,5);
 
 /////////////////////////////////////
 /* Problem 4
-Write a function called sumArray() that takes in an array of numbers as its single argument and then returns an array where the first element is the sum of the numbers in the array, and the second element is a string that EXACTLY follows this example and concatenates a message using the arguments that were passed into the function:
+Write a function called sumArray() that takes in an array of numbers as its single argument
+and then returns an array where the first element is the sum of the numbers in the array, and the second element is a string that EXACTLY follows this example and concatenates a message using the arguments that were passed into the function:
 
 "2,3,4 was passed in as an array of numbers, and 9 is their sum."
 
@@ -86,12 +87,27 @@ Test this function by hand in the console to get it working, and when you think 
 // TODO: Write your code here
 var testArray = [2,3,4]; //eslint-disable-line
 
-function sumArray(testArray){ //eslint-disable-line
+function sumArray(testArray){
+  var sumOne = sum(testArray[0],testArray[1]);
+  //console.log(sumOne[0]);
+  var sumTwo = sum(sumOne[0],testArray[2]);
+  console.log(sumTwo[0]);
 
+  //console.log(testArray[0]);
+  //console.log(testArray[1]);
+  //console.log(testArray[2]);
+  var messageOne = testArray[0] + ',' + testArray[1] + ',' + testArray[2] + ' was passed in as an array of numbers, and ' + sumTwo[0] + ' is their sum.';
+  //return [sumTwo[0], messageOne];
+  //alert(messageOne);
+  //console.log(messageOne);
+  return [sumTwo[0],messageOne];
 }
 
+
+sumArray(testArray);
+
 // TODO: Here is the test for sumArray(); uncomment it to run it
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
